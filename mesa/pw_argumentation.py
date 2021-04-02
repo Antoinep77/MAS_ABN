6b63039570b36c6ab7312166a092e13381af4130
Antoine Paris <antoineparis77@gmail.com>
2021-04-02 15:48:10 +0200

    add first messages

--- mesa/pw_argumentation.py
+++ mesa/pw_argumentation.py
@@ -3,6 +3,8 @@ from mesa.time import RandomActivation
 
 from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
+from communication.message.Message import Message
+from communication.message.MessagePerformative import MessagePerformative
 from communication.preferences.Preferences import Preferences
 from communication.preferences.Item import Item
 from communication.preferences.CriterionName import CriterionName
@@ -21,14 +23,23 @@ class ArgumentAgent(CommunicatingAgent):
         super().__init__(unique_id, model, name)
         self.preference = Preferences()
         self.preference.set_criterion_name_list(criterions)
+        self.items = []
 
     def step(self):
         super().step()
+        messages = filter(lambda message: message.performative == MessagePerformative.PROPOSE,self.get_new_messages())
+        for m in messages:
+            if self.preference.is_item_among_top_10_percent(m.get_content(),self.items):
+                self.send_message(self,m.get_exp(),MessagePerformative.ACCEPT,m.get_content())
+            else:
+                self.send_message(self,m.get_exp(),MessagePerformative.ASK_WHY,m.get_content())
+
 
     def get_preference(self):
         return self.preference
 
     def generate_random_preferences(self, item):
+        self.items.append(item)
         for criterion in self.preference.get_criterion_name_list():
             value = self.model.random.choice(list(Value))
             self.preference.add_criterion_value(CriterionValue(item,criterion,value))
@@ -50,13 +61,12 @@ class ArgumentModel(Model):
         for item in items:
             agent1.generate_random_preferences(item)
             agent2.generate_random_preferences(item)
-
         
         self.schedule.add(agent1)
         self.schedule.add(agent2)
-
-
         self.running = True
+
+        agent1.send_message(Message(agent1,agent2,MessagePerformative.PROPOSE,items[0]))
 
     def step(self):
         self.__messages_service.dispatch_messages()
